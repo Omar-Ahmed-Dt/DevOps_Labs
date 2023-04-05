cd576489105132bf00f0572a9bba4255b9ef5f88
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2023-04-06 00:12:37 +0200

    modified terraform project 2

--- terraform_project2/main.tf
+++ terraform_project2/main.tf
@@ -4,17 +4,54 @@ provider "aws" {
 }
 
 # Configure EC2 
-resource "aws_instance" "srvr" {
-  ami           = "ami-0aa2b7722dc1b5612"
-  instance_type = "t2.micro"
+# resource "aws_instance" "srvr" {
+#   ami           = "ami-0aa2b7722dc1b5612"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "ubuntu"
-  }
-}
+#   tags = {
+#     Name = "ubuntu"
+#   }
+# }
 
 # Configure VPC 
-resource "aws_vpc" "first-vpc" {
+# resource "aws_vpc" "first-vpc" {
+#   cidr_block = "10.0.0.0/16"
+
+#   tags = {
+#     Name = "production"
+#   }
+# }
+
+# resource "aws_vpc" "second-vpc" {
+#   cidr_block = "10.1.0.0/16"
+
+#   tags = {
+#     Name = "Dev"
+#   }
+# }
+
+# Configure subnets 
+# resource "aws_subnet" "subnet1" {
+#   vpc_id     = aws_vpc.first-vpc.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "prod-subnet"
+#   }
+# }
+
+# resource "aws_subnet" "subnet2" {
+#   vpc_id     = aws_vpc.second-vpc.id
+#   cidr_block = "10.1.1.0/24"
+
+#   tags = {
+#     Name = "dev-subnet"
+#   }
+# }
+
+
+# # 1. Create vpc
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -22,29 +59,133 @@ resource "aws_vpc" "first-vpc" {
   }
 }
 
-resource "aws_vpc" "second-vpc" {
-  cidr_block = "10.1.0.0/16"
+# # 2. Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
 
   tags = {
-    Name = "Dev"
+    Name = "gw"
   }
 }
 
-# Configure subnets 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.first-vpc.id
+# # 3. Create Custom Route Table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+# # 4. Create a Subnet 
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.prod-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "prod-subnet"
   }
 }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id     = aws_vpc.second-vpc.id
-  cidr_block = "10.1.1.0/24"
+# # 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# # 6. Create Security Group to allow port 22,80,443
+resource "aws_security_group" "allow-web" {
+  name        = "allow-web-traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description      = "HTTPS Traffic"
+    # Allow ports range from 443 to 447 
+    # from_port        = 443
+    # to_port          = 447
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]   # Any ip can access it via this port 
+  }
+
+  ingress {
+    description      = "HTTP Traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH Traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    # Allow all ports in the egress direction: 
+    from_port        = 0
+    to_port          = 0
+    # Any Protocol:  
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "dev-subnet"
+    Name = "allow_web"
   }
 }
+
+# # 7. Create a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  # Any ip from range subnet: 
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow-web.id]
+
+}
+
+# # 8. Assign an elastic IP to the network interface created in step 7
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  # EIP may require IGW to exist prior to association. Use depends_on to set an explicit dependency on the IGW.
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+# # 9. Create Ubuntu server and install/enable apache2
+resource "aws_instance" "srvr" {
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "DT"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
